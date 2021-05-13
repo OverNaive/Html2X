873cc226bb8cf8dcc459d6cc40ad801068463d53
OverNaive <overnaive20@gmail.com>
2021-05-13 23:24:33 +0800

    feat: 实现 html-to-img 接口

--- src/html2x.go
+++ src/html2x.go
@@ -61,6 +61,51 @@ func htmlToPdf(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w, req, pdfPath)
 }
 
+func htmlToImg(w http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if ! strings.HasPrefix(req.Header.Get("Content-Type"), "application/json") {
+		http.Error(w, "Invalid content type", http.StatusUnsupportedMediaType)
+		return
+	}
+
+	var b requestBody
+
+	err := json.NewDecoder(req.Body).Decode(&b)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	now := strconv.FormatInt(time.Now().UnixNano(), 10)
+	htmlPath := strings.Join([]string{now, ".html"}, "")
+	imgPath := strings.Join([]string{now, ".jpg"}, "")
+
+	defer os.Remove(htmlPath)
+	defer os.Remove(imgPath)
+
+	err = os.WriteFile(htmlPath, []byte(b.Html), 0644)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	args := b.Args
+	args = append(args, htmlPath, imgPath)
+
+	cmd := exec.Command("wkhtmltoimage", args...)
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.ServeFile(w, req, imgPath)
+}
+
 func main() {
 	s := &http.Server{
 		Addr: ":8888",
@@ -69,6 +114,7 @@ func main() {
 	}
 
 	http.HandleFunc("/html-to-pdf", htmlToPdf)
+	http.HandleFunc("/html-to-img", htmlToImg)
 
 	log.Fatal(s.ListenAndServe())
 }
